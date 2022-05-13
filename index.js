c85d0ae932ceaa14a64a4d2a19baeb111b05f8f0
hassan motiwala <muhammad.hassanmoti@gmail.com>
2022-05-13 21:02:57 +0500

    post api ready

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require("express");
 const app = express();
 const mongo = require("./mongodb");
 
+app.use(express.json());
+
 app.get("/", async (req, resp) => {
   let data = await mongo();
   let result = await data.find().toArray();
   console.log(result);
   resp.send(result);
 });
+app.post("/", async (req,resp)=>{
+  let data = await mongo();
+  let result = await data.insert(req.body)
+  resp.send(result);
+})
+
 app.listen(5000);
 
